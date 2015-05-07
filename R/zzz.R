4c83be736bbe729a3d4316eeacca506a7696c542
Joshua Schwab <joshuaschwab@yahoo.com>
2015-05-07 22:00:11 +0000

    version 0.9-5

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,5 @@
-.onAttach <- function(...) {
-  packageStartupMessage(
-"The influence curve based variance estimator implemented in this package provides 
-asymptotically conservative standard error estimates when the treatment mechanism is 
-estimated with MLE using a correctly specific parametric model. However, we have found 
-in simulations that in finite samples this variance estimator may be substantially
-anti-conservative in settings where the outcome is rare or there are positivity 
-violations. An improved variance estimator addressing these challenges is currently 
-under development and will be released in a subsequent version of the package.\n")
+release_questions <- function() {
+  c('Have you run    check(\'ltmle-dev\', cran = F)  ?', #do not skip any tests
+    'Have you run    run_examples(\'ltmle-dev\', test=F)   ?', #do not skip any examples
+    'Have you checked for nonASCII characters? - see R packages Evernote') 
 }
