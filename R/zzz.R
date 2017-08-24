225c1cb43e04f0d162f248721751f757ab58373f
Joshua Schwab <joshuaschwab@yahoo.com>
2017-08-24 16:16:14 +0000

    version 0.9-10

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,21 @@
 #...
 #search for fixme, browser
-#run: create tests to compare versions.R (check that it works with niter=1, then run again with niter=10)
+#run: create tests to compare versions.R (check that it works with niter=1, then run again with niter=10, then back to niter=1)
 #move test-prevRelease to ltmle-dev/tests/testthat
 #test("ltmle-dev")
-#run: check coverage 
-#source('~/Dropbox (UC Berkeley Biostat)/Josh-Berkeley/ltmle development/check coverage.R'
+#run: check coverage (done, but not sure about above)
+#source('~/Dropbox (UC Berkeley Biostat)/Josh-Berkeley/ltmle development/check coverage.R')
 #move test-prevRelease.R back to /ltmle development
-#run: CheckBeforeSubmit
+#document("ltmle-dev")
+#run: CheckBeforeSubmit:
+#  source('~/Dropbox (UC Berkeley Biostat)/Josh-Berkeley/utils/GeneralUtilities.R')
+#  CheckBeforeSubmit("ltmle-dev")
+#revdep_check("ltmle-dev")
 #update cran-comments.rd
 #update in github
-#build_win
-#submit
+#build_win("ltmle-dev", version = "R-release")
+#build_win("ltmle-dev", version = "R-devel")
+#release("ltmle-dev)
 #once accepted, tag in github as accepted on CRAN
 
 release_questions <- function() {
